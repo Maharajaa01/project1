174e45d75896b3ef74bfa14fe80d609d3b377899
Maharaja <114376134+Maharajaa01@users.noreply.github.com>
2024-02-20 12:04:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+x =int(input())
+if x > 70:
+   print ("Order Limit reached")
+else:
+   print("order confirmed")
+print("Thank YOU!")
+
+x =int(input())
+if x <= 70:
+   print ("Order Limit reached")
+else:
+   print("order  confirded")
+print("Thank YOU!")
